77be1dbb9fd7c44f067d54746a517af063cfcab0
Henrik Laurits Pedersen <henr985a@edu.eal.dk>
2018-05-01 14:28:13 +0200

    all

--- RichText/MainWindow.xaml.cs
+++ RichText/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace RichText
 {
@@ -20,9 +22,34 @@ namespace RichText
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public void BasicRichTextBoxWithContentExample()
         {
-            InitializeComponent();
+            StackPanel myStackPanel = new StackPanel();
+
+            // Create a FlowDocument to contain content for the RichTextBox.
+            FlowDocument myFlowDoc = new FlowDocument();
+
+            // Create a Run of plain text and some bold text.
+            Run myRun = new Run("This is flow content and you can ");
+            Bold myBold = new Bold(new Run("edit me!"));
+
+            // Create a paragraph and add the Run and Bold to it.
+            Paragraph myParagraph = new Paragraph();
+            myParagraph.Inlines.Add(myRun);
+            myParagraph.Inlines.Add(myBold);
+
+            // Add the paragraph to the FlowDocument.
+            myFlowDoc.Blocks.Add(myParagraph);
+
+            RichTextBox myRichTextBox = new RichTextBox();
+
+            // Add initial content to the RichTextBox.
+            myRichTextBox.Document = myFlowDoc;
+
+            myStackPanel.Children.Add(myRichTextBox);
+            this.Content = myStackPanel;
+
         }
     }
 }
+
